a8d4669af3602b2baadeda55cd398dd1c6bb0154
gweltaz-calori <andoracontact@gmail.com>
2019-02-05 19:02:18 +0100

    first commit

--- Droooni/CameraViewController.swift
+++ Droooni/CameraViewController.swift
@@ -7,24 +7,99 @@
 //
 
 import UIKit
-
-class CameraViewController: UIViewController {
-
+import Vision
+import CoreML
+import AVFoundation
+class CameraViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    private var requests = [VNRequest]()
+    @IBOutlet weak var cameraView: UIView!
+    // Create a layer to display camera frames in the UIView
+    private lazy var cameraLayer: AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer(session: self.captureSession)
+    // Create an AVCaptureSession
+    private lazy var captureSession: AVCaptureSession = {
+        let session = AVCaptureSession()
+        session.sessionPreset = AVCaptureSession.Preset.photo
+        guard
+            let backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back),
+            let input = try? AVCaptureDeviceInput(device: backCamera)
+            else { return session }
+        session.addInput(input)
+        return session
+    }()
+    
+    private lazy var classifier: SparkClassifier = SparkClassifier()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        cameraView?.layer.addSublayer(cameraLayer)
+        cameraLayer.frame = cameraView.bounds
+        
+        //        cameraLayer.videoGravity = .resizeAspectFill
+        let videoOutput = AVCaptureVideoDataOutput()
+        videoOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as String: Int(kCVPixelFormatType_32BGRA)]
+        videoOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "MyQueue"))
+        self.captureSession.addOutput(videoOutput)
+        self.captureSession.startRunning()
         // Do any additional setup after loading the view.
+        
+        setupVision()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func setupVision() {
+        guard let visionModel = try? VNCoreMLModel(for: classifier.model) else {
+            fatalError("Can’t load VisionML model")
+        }
+        let classificationRequest = VNCoreMLRequest(model: visionModel, completionHandler: handleClassifications)
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.scaleFill
+        requests = [classificationRequest]
     }
-    */
+   
+    
+    func handleClassifications(request: VNRequest, error: Error?) {
+        guard let results = request.results else { return }
+        
+        for case let foundObject as VNRecognizedObjectObservation in results {
+            let bestLabel = foundObject.labels.first! // Label with highest confidence
+            let objectBounds = foundObject.boundingBox
+            
+            print(bestLabel.identifier, bestLabel.confidence, objectBounds)
+            
+            
+            highlight(boundingRect: objectBounds)
+        }
+        
+    }
+    
+    func highlight(boundingRect: CGRect) {
+        let source = self.cameraView.frame
+        
+        let rectWidth = source.size.width * boundingRect.size.width
+        let rectHeight = source.size.height * boundingRect.size.height
+        
+        let outline = CALayer()
+        outline.frame = CGRect(x: boundingRect.origin.x * source.size.width, y:boundingRect.origin.y * source.size.height, width: rectWidth, height: rectHeight)
+        
+        
+        outline.borderWidth = 2.0
+        outline.borderColor = UIColor.red.cgColor
+        
+        self.cameraLayer.addSublayer(outline)
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let pixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+            return
+        }
+        var requestOptions:[VNImageOption : Any] = [:]
+        if let cameraIntrinsicData = CMGetAttachment(sampleBuffer, key: kCMSampleBufferAttachmentKey_CameraIntrinsicMatrix, attachmentModeOut: nil) {
+            requestOptions = [.cameraIntrinsics:cameraIntrinsicData]
+        }
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: CGImagePropertyOrientation(rawValue: 6)!, options: requestOptions)
+        do {
+            try imageRequestHandler.perform(self.requests)
+        } catch {
+            print(error)
+        }
+    }
 
 }
--- Droooni/ViewController.swift
+++ Droooni/ViewController.swift
@@ -7,14 +7,156 @@
 //
 
 import UIKit
+import CoreML
+import Vision
+import ImageIO
+
+extension CGPoint {
+    func scaled(to size: CGSize) -> CGPoint {
+        return CGPoint(x: self.x * size.width, y: self.y * size.height)
+    }
+}
+extension CGRect {
+    func scaled(to size: CGSize) -> CGRect {
+        return CGRect(
+            x: self.origin.x * size.width,
+            y: self.origin.y * size.height,
+            width: self.size.width * size.width,
+            height: self.size.height * size.height
+        )
+    }
+}
+
+extension CGImagePropertyOrientation {
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .up: self = .up
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        }
+    }
+}
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var classificationLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    var inputImage: CIImage?
+    
+    var nmsThreshold: Float = 0.5
 
+    @IBAction func click(_ sender: Any) {
+        performSegue(withIdentifier: "cameraId", sender: self)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        guard let image = UIImage(contentsOfFile: Bundle.main.path(forResource: "dji9", ofType: "jpg")!) else {
+            return
+        }
+        guard let ciImage = CIImage(image: image)
+            else { fatalError("can't create CIImage from UIImage") }
+        
+        let orientation = CGImagePropertyOrientation(image.imageOrientation)
+        
+        inputImage = ciImage.oriented(forExifOrientation: Int32(orientation.rawValue))
+        
+        imageView.image = image
+        
+        let handler = VNImageRequestHandler(ciImage: ciImage, orientation: orientation)
+        DispatchQueue.global(qos: .userInteractive).async {
+            do {
+                try handler.perform([self.detectionRequest])
+            } catch {
+                print(error)
+            }
+        }
     }
 
+    lazy var detectionRequest: VNCoreMLRequest = {
+        // Load the ML model through its generated class and create a Vision request for it.
+        do {
+            let model = try VNCoreMLModel(for: SparkClassifier().model)
+            
+            return VNCoreMLRequest(model: model, completionHandler: self.handleDetection)
+        } catch {
+            fatalError("can't load Vision ML model: \(error)")
+        }
+    }()
+    
+    func handleDetection(request: VNRequest, error: Error?) {
+        
+        guard let results = request.results else { return }
+        
+        print(results)
+        
+        for case let foundObject as VNRecognizedObjectObservation in results {
+            let bestLabel = foundObject.labels.first! // Label with highest confidence
+            let objectBounds = foundObject.boundingBox
+            
+            print(bestLabel.identifier, bestLabel.confidence, objectBounds)
+            
+            
+            
+            drawRectangle(detectedRectangle: objectBounds)
+        }
+        
+        
+    }
+    
 
+    public func drawRectangle(detectedRectangle: CGRect) {
+        guard let inputImage = inputImage else {
+            return
+        }
+        // Verify detected rectangle is valid.
+        let boundingBox = detectedRectangle.scaled(to: inputImage.extent.size)
+        
+        
+        // Show the pre-processed image
+        DispatchQueue.main.async {
+            self.imageView.image = self.drawOnImage(source: self.imageView.image!, boundingRect: detectedRectangle)
+        }
+    }
+    
+    
+    fileprivate func drawOnImage(source: UIImage,
+                                 boundingRect: CGRect) -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(source.size, false, 1)
+        let context = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: source.size.width, y: 0)
+        context.scaleBy(x: -1.0, y: 1.0)
+        context.setLineJoin(.round)
+        context.setLineCap(.round)
+        context.setShouldAntialias(true)
+        context.setAllowsAntialiasing(true)
+        
+        let rectWidth = source.size.width * boundingRect.size.width
+        let rectHeight = source.size.height * boundingRect.size.height
+        
+        //draw image
+        let rect = CGRect(x: 0, y:0, width: source.size.width, height: source.size.height)
+        context.draw(source.cgImage!, in: rect)
+        
+        
+        //draw bound rect
+        var fillColor = UIColor.green
+        fillColor.setFill()
+        context.addRect(CGRect(x: boundingRect.origin.x * source.size.width, y:boundingRect.origin.y * source.size.height, width: rectWidth, height: rectHeight))
+        
+        //draw overlay
+        fillColor = UIColor.red
+        fillColor.setStroke()
+        context.setLineWidth(12.0)
+        context.drawPath(using: CGPathDrawingMode.stroke)
+        
+        let coloredImg : UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return coloredImg
+    }
 }
 
